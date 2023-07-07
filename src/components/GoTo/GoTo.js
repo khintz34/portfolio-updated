749cc10ceef4bf75a9d788851d2091b9c46307e6
Kevin Hintz <khintz34@gmail.com>
2023-07-06 19:52:48 -0500

    section clicks

--- src/components/GoTo/GoTo.js
+++ src/components/GoTo/GoTo.js
@@ -1,11 +1,12 @@
+"use client";
 import react from "react";
 import styles from "./GoTo.module.scss";
 
-export default function GoTo({ title }) {
+export default function GoTo({ title, status, onClick }) {
   return (
-    <div className={styles.sectionDiv}>
-      <hr className={styles.line} />
-      <h4 className={styles.h4}>{title}</h4>
+    <div className={styles.sectionDiv} onClick={onClick}>
+      <hr className={status ? styles.clicked : styles.line} />
+      <h4 className={status ? styles.h4Clicked : styles.h4}>{title}</h4>
     </div>
   );
 }
--- src/components/LeftMain/LeftMain.js
+++ src/components/LeftMain/LeftMain.js
@@ -1,10 +1,29 @@
-import react from "react";
+"use client";
+import react, { useState } from "react";
 import GoTo from "../GoTo/GoTo";
 import styles from "../LeftMain/LeftMain.module.scss";
 import { FaLinkedin, FaGithub } from "react-icons/fa";
 import { IconContext } from "react-icons";
 
 export default function LeftMain() {
+  const [aboutSection, setAboutSection] = useState(true);
+  const [projectSection, setProjectSection] = useState(false);
+  const [expSection, setExpSection] = useState(false);
+
+  function handleClick(item) {
+    setAboutSection(false);
+    setProjectSection(false);
+    setExpSection(false);
+
+    if (item === "about") {
+      setAboutSection(true);
+    } else if (item === "project") {
+      setProjectSection(true);
+    } else {
+      setExpSection(true);
+    }
+  }
+
   return (
     <main className={styles.main}>
       <div>
@@ -15,9 +34,21 @@ export default function LeftMain() {
         </h3>
       </div>
       <div className={styles.goTos}>
-        <GoTo title="About" />
-        <GoTo title="Projects" />
-        <GoTo title="Experience" />
+        <GoTo
+          title="About"
+          status={aboutSection}
+          onClick={() => handleClick("about")}
+        />
+        <GoTo
+          title="Projects"
+          status={projectSection}
+          onClick={() => handleClick("project")}
+        />
+        <GoTo
+          title="Experience"
+          status={expSection}
+          onClick={() => handleClick("exp")}
+        />
       </div>
       <div className={styles.contacts}>
         <FaLinkedin className={styles.icon} />
