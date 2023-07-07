8fb868cbb9d564b2a3d7c5f7876844493797e79c
Kevin Hintz <khintz34@gmail.com>
2023-07-07 11:05:14 -0500

    proj basic

--- src/components/Project/Project.js
+++ src/components/Project/Project.js
@@ -3,19 +3,23 @@ import Image from "next/image";
 import react from "react";
 import styles from "./Project.module.scss";
 
+// todo fix border around image
+
 export default function Project({ image, alt, title, about, tech }) {
   return (
     <main className={styles.projectMain}>
-      <Image
-        src={image}
-        width={225}
-        height={75}
-        alt={alt}
-        className={styles.image}
-      />
+      <div className={styles.imageBorder}>
+        <Image
+          src={image}
+          width={225}
+          height={75}
+          alt={alt}
+          className={styles.image}
+        />
+      </div>
       <div className={styles.projectInfo}>
         <h4 className={styles.h4}>{title}</h4>
-        <p>{about}</p>
+        <p className={styles.about}>{about}</p>
         <div className={styles.techContainer}>
           {tech.map((val) => {
             return (
--- src/components/ProjectContainer/ProjectContainer.js
+++ src/components/ProjectContainer/ProjectContainer.js
@@ -3,16 +3,32 @@ import react from "react";
 import Project from "../Project/Project";
 import styles from "./ProjectContainer.module.scss";
 import milk from "../../assets/images/milk.png";
+import hipcamp from "../../assets/images/hipcamp.png";
+import football from "../../assets/images/footballapi.png";
 
 export default function ProjectContainer() {
   return (
     <main className={styles.projectContainer}>
       <Project
         image={milk}
-        title="Milk Storage Calculator"
+        title="Pump End Date Calculator"
         about="This was a project I made for my wife to find out when she can be done pumping based on critiera like stored milk, oz needed per day, and oz saved, and milk end date. It was created using React and Next.js, and is deployed through Vercel."
-        alt="Milk Stoage Calculator"
+        alt="Pump End Date Calculator"
         tech={["React", "Next.js", "Vercel"]}
+      />
+      <Project
+        image={hipcamp}
+        title="Hipcamp Clone"
+        about="This site is a clone of the popular camping website, Hipcamp. It utilizes Google Firebase to read and write new campsites from the database to the UI. Users can search for campsites via name, state, city, numbers of guests, pet friendly status and more. After one finds the perfect site they can book it. Additionally, users who would like to add new campsites to the database can do so if authenticated. This site was built using React.js. "
+        alt="Hipcamp Clone Image"
+        tech={["React", "GH Pages", "Google Firebase"]}
+      />
+      <Project
+        image={football}
+        title="EPL Team Tracker"
+        about="This project was made to practice using APIs in a variety of different ways. It was created using React, Next.js, and API-Football from RapidAPI. The basic premise is you can find stats, schedules, rosters, and standings from each team in the English Premier League."
+        alt="EPL Team Tracker Image"
+        tech={["React", "Next.js", "Vercel", "RapidAPI"]}
       />
     </main>
   );
