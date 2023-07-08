99611bf5c0d3d8b2e7b769b78be8278042547a31
Kevin Hintz <khintz34@gmail.com>
2023-07-08 09:51:02 -0500

    small change for leftExp

--- src/components/Experience/Experience.js
+++ src/components/Experience/Experience.js
@@ -3,6 +3,9 @@ import Image from "next/image";
 import react from "react";
 import styles from "./Experience.module.scss";
 
+// todo change topExp to leftExp and have that just be image
+// todo change bttomExp to rightExp and make that flex column
+
 export default function Experience({
   title,
   company,
@@ -12,11 +15,10 @@ export default function Experience({
   dates,
   alt,
 }) {
-  console.log(image);
   return (
     <main className={styles.experienceMain}>
-      <div className={styles.topExp}>
-        <div className={styles.image}>
+      <div className={styles.leftExp}>
+        <div className={styles.imageContainer}>
           {/* <Image
             src={image}
             width={80}
@@ -26,17 +28,19 @@ export default function Experience({
           /> */}
           <img src={image.src} alt="" className={styles.image} />
         </div>
-        <div>
-          <div className={styles.info}>
-            <p>{title}</p>
-            <p>-</p>
-            <p>{company}</p>
-          </div>
-          <div>{dates}</div>
-        </div>
       </div>
-      <div className="bottomExp">
-        <p>{para}</p>
+      <div className={styles.rightExp}>
+        <div className={styles.rando}>
+          <div className={styles.info}>
+            <p className={styles.title}>{title}</p>
+            <p className={styles.interpunct}>·</p>
+            <p className={styles.title}>{company}</p>
+          </div>
+          <div className={styles.dates}>{dates}</div>
+        </div>
+        <div className={styles.bottomExp}>
+          <p className={styles.para}>{para}</p>
+        </div>
       </div>
     </main>
   );
