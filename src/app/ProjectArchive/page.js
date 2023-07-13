3e11e04c9a7255817ab9b7c3359a5cad55640bb8
Kevin Hintz <khintz34@gmail.com>
2023-07-13 13:10:11 -0500

    more proj

--- src/app/ProjectArchive/page.js
+++ src/app/ProjectArchive/page.js
@@ -6,6 +6,9 @@ import Project from "../../components/Project/Project";
 import milk from "../../assets/images/milk.png";
 import hipcamp from "../../assets/images/hipcamp.png";
 import football from "../../assets/images/footballapi.png";
+import ispy from "../../assets/images/ispy.png";
+import workout from "../../assets/images/workout.png";
+import dadmode from "../../assets/images/dadmode.png";
 
 export default function ProjectArchive() {
   return (
@@ -40,6 +43,27 @@ export default function ProjectArchive() {
           about="This project was made to practice using APIs in a variety of different ways. It was created using React, Next.js, and API-Football from RapidAPI. The basic premise is you can find stats, schedules, rosters, and standings from each team in the English Premier League."
           alt="EPL Team Tracker Image"
           tech={["React", "Next.js", "Vercel", "RapidAPI"]}
+        />
+        <Project
+          image={ispy}
+          title="I Spy"
+          about="I Spy is an interative game where users can see how quickly they can find a list of items in an image. Featuring six different levels and a global leaderboard for each image, they can see how they stack up againt the competition. I Spy was created with React.js and Google Firebase."
+          alt="I Spy Image"
+          tech={["React", "Google Firebase"]}
+        />
+        <Project
+          image={dadmode}
+          title="Dad Mode"
+          about="Dad Mode is an online shopping site where dads from all walks of life can find solace by buying dad shoes, jorts, lawn mowers, or even fanny packs. As a user shops they can add items to their cart and see their goods and total price when they go to check out. Dad Mode was created using React.js. "
+          alt="Dad Mode Image"
+          tech={["React"]}
+        />
+        <Project
+          image={workout}
+          title="Workout Generator"
+          about="I found myself doing the same workouts over and over again with little variation. Even when I tried a new exercise I would forget about it the next time I lifted. So I created a workout generator app that gives you a list of exercises based on muscle groups or exercise types that you input. It randomizes the returned result so you won't be doing the same workout twice in a row. This workout generator was created using React, GH Pages, and the Exercise API from API-Ninja."
+          alt="Workout Generator Image"
+          tech={["React", "GH Pages", "API-Ninja"]}
         />
       </div>
     </main>
