840f59995846a22d769eeaa86ec47d54af920901
Kevin Hintz <khintz34@gmail.com>
2023-07-24 14:54:39 -0500

    image

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,11 +1,13 @@
 import "./globals.css";
 import { Inter } from "next/font/google";
+import pic from "../assets/images/og.png";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Kevin's Portfolio",
   description: "Frontend Software Engineer",
+  image: { pic },
 };
 
 export default function RootLayout({ children }) {
