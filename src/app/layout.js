444c6049eeec6677ada6d19997d6a083a071e5d3
Kevin Hintz <khintz34@gmail.com>
2023-07-24 15:15:01 -0500

    og image 2

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,6 @@
 import "./globals.css";
 import { Inter } from "next/font/google";
-import pic from "../assets/images/og.png";
+import { pic } from "../assets/images/og.png";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,6 +15,14 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <head>
         <meta property="og:image" content="../assets/images/og.png" />
+        <meta
+          property="og:title"
+          content="Kevin Hintz's Engineering Portfolio"
+        />
+        <meta
+          property="og:description"
+          content="Kevin Hintz Frontend Engineer"
+        />
       </head>
       <body className={inter.className}>{children}</body>
     </html>
